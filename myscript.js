ef90bb9e270da1e7c76e0f1d1edcbc3d3a9276b2
mike <mikecee0815@gmail.com>
2018-02-12 22:12:27 -0500

    2nd commit

--- myscript.js
+++ myscript.js
@@ -1,11 +1,14 @@
 
 window.onload() {
-	var url = "http://github.com/mikecee0815/beatles.json";
+
+	var url = "https://mikecee0815.github.io/beatles/beatles.json";
 	var request = new XMLHttpRequest();
 	request.open("GET",url);
 	request.onload = function(){
 	if (request.status == 200) {
 		alert.("Data succesfully recieved!!!");
+	} else {
+		alert("no data...");
 	}
 };
 
