8beafb347583a78776b3a63bca73ddb17711846a
mike <mikecee0815@gmail.com>
2018-02-13 23:04:16 -0500

    jsonp phase 9

--- beatles.js
+++ beatles.js
@@ -1,8 +1,8 @@
  
  
  var beatleCars = [ {"name": "Herby","year":"1967"},
-   {"name": "Groovy","year":"1964"},
-   {"name": "Funky","year":"1973"}
- ]
+   		      {"name": "Groovy","year":"1964"},
+   		      {"name": "Funky","year":"1973"}
+ ];
 
  beatles(beatleCars);
