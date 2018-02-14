9be5fda2d0b180ad486cddd777d8187a41667bc6
mike <mikecee0815@gmail.com>
2018-02-13 21:20:06 -0500

    jsonp phase one

--- myscript.js
+++ myscript.js
@@ -1,30 +1,18 @@
 
 window.onload = function() {
 
-	var url = "https://mikecee0815.github.io/beatles/beatles.json";
-	var request = new XMLHttpRequest();
-	request.open("GET",url);
-	request.onload = function(){
-	if (request.status == 200) {
-		fetchSomething(request.responseText);
-		// alert(request.responseText);
-	} else {
-		alert("no data...");
-	}
-};
-request.send(null);
 
 }
 
-function fetchSomething(response){
+function beatles(cars){
 	var dataDiv = document.getElementById('dataDisplay');
-	var dataSet = JSON.parse(response);
-	for(i = 0; i <= dataSet.length; i++){
-		var dataItem = dataSet[i];
+	
+	for(i = 0; i <= cars.length; i++){
+		var car = cars[i];
 		var li = document.createElement('li');
-		li.innerHTML = dataItem.year;
+		li.innerHTML = car.year;
 		dataDiv.appendChild(li);
-		console.log(dataSet);
+		// console.log(dataSet);
 	}
 	
 }
